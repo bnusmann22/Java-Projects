6a2f4dd7a89b0a4ccf3535472cd34ed52c62203d
Abdullahi Jamil Muhammad <bn.usmann22@gmail.com>
2025-11-27 15:50:01 +0100

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,25 @@
+// public class Hello {
+//   public static void main(String[] args) {
+//     System.out.println("Hello World");
+//   }
+// }
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Im Mastering JAVA 🥳");
+
+        String school ="Bayero University";
+        System.out.println(school);
+
+        int myNum;
+        myNum = 15;
+        System.out.println(myNum);
+
+        final int myConstNum = 15;
+        System.out.println(myConstNum);
+
+
+    }
+}
